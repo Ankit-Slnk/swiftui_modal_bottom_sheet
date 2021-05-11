3da3037b1eab1888ddb91b19dc7e5219f04cedb0
Ankit Solanki <ankit_ms95@yahoo.com>
2021-05-11 07:40:39 +0530

    Initial Commit

---
+++ Modal Bottom Sheet Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modal Bottom Sheet Demo
+//
+//  Created by Ankit Solanki on 11/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modal Bottom Sheet Demo/Modal_Bottom_Sheet_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modal_Bottom_Sheet_DemoApp.swift
+//  Modal Bottom Sheet Demo
+//
+//  Created by Ankit Solanki on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Modal_Bottom_Sheet_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
